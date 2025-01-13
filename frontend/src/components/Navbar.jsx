62de1afd4f0816ce24174d3163900a41136f28e4
Rishiraj Gupta <grishi349@gmail.com>
2025-01-13 23:08:39 +0530

    [frontend] my-appointments page completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,29 +36,49 @@ const Navbar = () => {
       </ul>
       <div>
         {token ? (
-          <div className="flex items-center gap-2 cursor-pointer group relative">
-            <img
-              className="w-8 rounded-full"
-              src={assets.profile_pic}
-              alt=" "
-            />
-            <img className="w-2.5" src={assets.dropdown_icon} alt="" />
-            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-500 z-20 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out transform group-hover:translate-y-0 translate-y-2">
+          <div className="flex items-center gap-2 cursor-pointer relative">
+            <div
+              onClick={() => setShowMenu(!showMenu)}
+              className="flex items-center gap-2"
+            >
+              <img
+                className="w-8 rounded-full"
+                src={assets.profile_pic}
+                alt=" "
+              />
+              <img className="w-2.5" src={assets.dropdown_icon} alt="" />
+            </div>
+            <div
+              className={`absolute top-0 right-0 pt-14 text-base font-medium text-gray-500 z-20 transition-all duration-300 ease-in-out transform ${
+                showMenu
+                  ? "opacity-100 visible translate-y-0"
+                  : "opacity-0 invisible translate-y-2"
+              }`}
+            >
               <div className="min-w-44 bg-gray-100 flex flex-col gap-4 p-4 rounded-xl shadow-lg">
                 <p
-                  onClick={() => navigate("/my-profile")}
+                  onClick={() => {
+                    navigate("/my-profile");
+                    setShowMenu(false);
+                  }}
                   className="hover:text-primary transition-colors duration-200 cursor-pointer"
                 >
                   My Profile
                 </p>
                 <p
-                  onClick={() => navigate("/my-appointments")}
+                  onClick={() => {
+                    navigate("/my-appointments");
+                    setShowMenu(false);
+                  }}
                   className="hover:text-primary transition-colors duration-200 cursor-pointer"
                 >
                   My Appointments
                 </p>
                 <p
-                  onClick={() => setToken(false)}
+                  onClick={() => {
+                    setToken(false);
+                    setShowMenu(false);
+                  }}
                   className="hover:text-primary transition-colors duration-200 cursor-pointer"
                 >
                   Logout
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,7 +1,95 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const MyAppointments = () => {
-  return <div>MyAppointments</div>;
+  const { doctors } = useContext(AppContext);
+
+  return (
+    <div>
+      <p className="pb-3 mt-12 text-lg font-medium text-gray-600 border-b">
+        My appointments
+      </p>
+      <div className="">
+        {doctors.slice(0, 2).map((item, index) => (
+          <div
+            key={index}
+            className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-4 border-b"
+          >
+            <div>
+              <img className="w-36 bg-[#EAEFFF]" src={item.image} alt="" />
+            </div>
+            <div className="flex-1 text-sm text-[#5E5E5E]">
+              <p className="text-[#262626] text-base font-semibold">
+                {item.name}
+              </p>
+              <p>{item.speciality}</p>
+              <p className="text-[#464646] font-medium mt-1">Address:</p>
+              <p className="">{item.address.line1}</p>
+              <p className="">{item.address.line2}</p>
+              <p className=" mt-1">
+                <span className="text-sm text-[#3C3C3C] font-medium">
+                  Date & Time:
+                </span>{" "}
+                25th Jan 2025, 10:00 AM
+              </p>
+            </div>
+            <div></div>
+            <div className="flex flex-col gap-2 justify-end text-sm text-center">
+              <button
+                onClick={() => setPayment(item._id)}
+                className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300"
+              >
+                Pay Online
+              </button>
+
+              <button
+                onClick={() => appointmentStripe(item._id)}
+                className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-gray-100 hover:text-white transition-all duration-300 flex items-center justify-center"
+              >
+                <img
+                  className="max-w-20 max-h-5"
+                  src={assets.stripe_logo}
+                  alt=""
+                />
+              </button>
+
+              <button
+                onClick={() => appointmentRazorpay(item._id)}
+                className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-gray-100 hover:text-white transition-all duration-300 flex items-center justify-center"
+              >
+                <img
+                  className="max-w-20 max-h-5"
+                  src={assets.razorpay_logo}
+                  alt=""
+                />
+              </button>
+
+              <button className="sm:min-w-48 py-2 border rounded text-[#696969]  bg-[#EAEFFF]">
+                Paid
+              </button>
+
+              <button className="sm:min-w-48 py-2 border border-green-500 rounded text-green-500">
+                Completed
+              </button>
+
+              <button
+                onClick={() => cancelAppointment(item._id)}
+                className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300"
+              >
+                Cancel appointment
+              </button>
+
+              <button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500">
+                Appointment cancelled
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
