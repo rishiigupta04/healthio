fbc5cfebae7590741dd0a686a039d37913c52c13
Rishiraj Gupta <grishi349@gmail.com>
2025-01-21 14:05:26 +0530

    added dynamic sidebar with all the Admin Panel routes and pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from "react";
 import Login from "./pages/Login";
-
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./components/NavBar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/admin/Dashboard";
+import AllAppointments from "./pages/admin/AllAppointments";
+import AddDoctor from "./pages/admin/AddDoctor";
+import DoctorList from "./pages/admin/DoctorsList";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -22,6 +27,16 @@ const App = () => {
         theme="light"
       />
       <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorList />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
