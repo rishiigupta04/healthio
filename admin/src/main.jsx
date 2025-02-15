d027204a19dbaebdca0daf17e22d81ab9376592b
Rishiraj Gupta <grishi349@gmail.com>
2025-02-15 13:00:27 +0530

    some changes

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,12 +1,13 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AdminContextProvider from "./context/AdminContext.jsx";
 import DoctorContextProvider from "./context/DoctorContext.jsx";
-import AppContextProvider from "./context/appContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AdminContextProvider>
       <DoctorContextProvider>
