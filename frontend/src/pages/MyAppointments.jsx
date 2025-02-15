355a4f535c1aae35a3125855d1a771baa75898d5
Rishiraj Gupta <grishi349@gmail.com>
2025-02-15 13:24:26 +0530

    ui display message

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -4,7 +4,6 @@ import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import { toast } from "react-toastify";
 import axios from "axios";
-import { use } from "react";
 
 const MyAppointments = () => {
   const { backendUrl, token, getDoctorsData } = useContext(AppContext);
@@ -167,99 +166,110 @@ const MyAppointments = () => {
         My appointments
       </p>
       <div className="">
-        {appointments.map((item, index) => (
-          <div
-            key={index}
-            className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-4 border-b"
-          >
-            <div>
-              <img
-                className="w-36 bg-[#EAEFFF]"
-                src={item.docData.image}
-                alt=""
-              />
-            </div>
-            <div className="flex-1 text-sm text-[#5E5E5E]">
-              <p className="text-[#262626] text-base font-semibold">
-                {item.docData.name}
-              </p>
-              <p>{item.docData.speciality}</p>
-              <p className="text-[#464646] font-medium mt-1">Address:</p>
-              <p className="">{item.docData.address.line1}</p>
-              <p className="">{item.docData.address.line2}</p>
-              <p className=" mt-1">
-                <span className="text-sm text-[#3C3C3C] font-medium">
-                  Date & Time:
-                </span>{" "}
-                {slotDateFormat(item.slotDate)} | {item.slotTime}
-              </p>
-            </div>
-
-            <div className="flex flex-col items-center justify-center gap-2 text-sm text-center">
-              {!item.cancelled &&
-                !item.payment &&
-                !item.isCompleted &&
-                item.payment !== item._id && (
-                  <button
-                    onClick={() => appointmentRazorpay(item._id)}
-                    className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300"
-                  >
-                    Pay Online
-                  </button>
-                )}
-              {!item.cancelled &&
-                !item.payment &&
-                !item.isCompleted &&
-                item.payment === item._id && (
-                  <button className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-gray-100 hover:text-white transition-all duration-300 flex items-center justify-center">
-                    <img
-                      className="max-w-20 max-h-5"
-                      src={assets.stripe_logo}
-                      alt=""
-                    />
-                  </button>
-                )}
-              {!item.cancelled &&
-                !item.payment &&
-                !item.isCompleted &&
-                item.payment === item._id && (
-                  <button
-                    onClick={() => appointmentRazorpay(item._id)}
-                    className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-gray-100 hover:text-white transition-all duration-300 flex items-center justify-center"
-                  >
-                    <img
-                      className="max-w-20 max-h-5"
-                      src={assets.razorpay_logo}
-                      alt=""
-                    />
-                  </button>
-                )}
-              {!item.cancelled && item.payment && !item.isCompleted && (
-                <button className="sm:min-w-48 py-2 border-2 border-green-500 rounded text-green-500  cursor-default">
-                  Paid
-                </button>
-              )}
-              {item.isCompleted && (
-                <button className="sm:min-w-48 py-2 bg-green-600 text-white rounded text-green cursor-default">
-                  Completed
-                </button>
-              )}
-              {!item.cancelled && !item.isCompleted && (
-                <button
-                  onClick={() => cancelAppointment(item._id)}
-                  className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300"
-                >
-                  Cancel appointment
-                </button>
-              )}
-              {item.cancelled && !item.isCompleted && (
-                <button className="sm:min-w-48 py-2 border-2  border-red-500 rounded text-red-500 cursor-default">
-                  Appointment cancelled
-                </button>
-              )}
-            </div>
+        {appointments.length === 0 ? (
+          <div className="text-center mt-10">
+            <p className="text-lg text-gray-500">
+              You have no appointments scheduled.
+            </p>
+            <p className="text-sm text-gray-400">
+              Please check back later or book a new appointment.
+            </p>
           </div>
-        ))}
+        ) : (
+          appointments.map((item, index) => (
+            <div
+              key={index}
+              className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-4 border-b"
+            >
+              <div>
+                <img
+                  className="w-36 bg-[#EAEFFF]"
+                  src={item.docData.image}
+                  alt=""
+                />
+              </div>
+              <div className="flex-1 text-sm text-[#5E5E5E]">
+                <p className="text-[#262626] text-base font-semibold">
+                  {item.docData.name}
+                </p>
+                <p>{item.docData.speciality}</p>
+                <p className="text-[#464646] font-medium mt-1">Address:</p>
+                <p className="">{item.docData.address.line1}</p>
+                <p className="">{item.docData.address.line2}</p>
+                <p className=" mt-1">
+                  <span className="text-sm text-[#3C3C3C] font-medium">
+                    Date & Time:
+                  </span>{" "}
+                  {slotDateFormat(item.slotDate)} | {item.slotTime}
+                </p>
+              </div>
+
+              <div className="flex flex-col items-center justify-center gap-2 text-sm text-center">
+                {!item.cancelled &&
+                  !item.payment &&
+                  !item.isCompleted &&
+                  item.payment !== item._id && (
+                    <button
+                      onClick={() => appointmentRazorpay(item._id)}
+                      className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300"
+                    >
+                      Pay Online
+                    </button>
+                  )}
+                {!item.cancelled &&
+                  !item.payment &&
+                  !item.isCompleted &&
+                  item.payment === item._id && (
+                    <button className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-gray-100 hover:text-white transition-all duration-300 flex items-center justify-center">
+                      <img
+                        className="max-w-20 max-h-5"
+                        src={assets.stripe_logo}
+                        alt=""
+                      />
+                    </button>
+                  )}
+                {!item.cancelled &&
+                  !item.payment &&
+                  !item.isCompleted &&
+                  item.payment === item._id && (
+                    <button
+                      onClick={() => appointmentRazorpay(item._id)}
+                      className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-gray-100 hover:text-white transition-all duration-300 flex items-center justify-center"
+                    >
+                      <img
+                        className="max-w-20 max-h-5"
+                        src={assets.razorpay_logo}
+                        alt=""
+                      />
+                    </button>
+                  )}
+                {!item.cancelled && item.payment && !item.isCompleted && (
+                  <button className="sm:min-w-48 py-2 border-2 border-green-500 rounded text-green-500  cursor-default">
+                    Paid
+                  </button>
+                )}
+                {item.isCompleted && (
+                  <button className="sm:min-w-48 py-2 bg-green-600 text-white rounded text-green cursor-default">
+                    Completed
+                  </button>
+                )}
+                {!item.cancelled && !item.isCompleted && (
+                  <button
+                    onClick={() => cancelAppointment(item._id)}
+                    className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300"
+                  >
+                    Cancel appointment
+                  </button>
+                )}
+                {item.cancelled && !item.isCompleted && (
+                  <button className="sm:min-w-48 py-2 border-2  border-red-500 rounded text-red-500 cursor-default">
+                    Appointment cancelled
+                  </button>
+                )}
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
